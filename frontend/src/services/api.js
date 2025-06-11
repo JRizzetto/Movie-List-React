68f098d808a3cf6db6d136b232611e6df069f108
jeffersonrizzetto@gmail.com <jeffersonrizzetto@gmail.com>
2025-06-11 08:38:02 -0300

    Projeto movie list finalizado

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "0cb26ce263b4f6f299c39d4c38c44f8c";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
